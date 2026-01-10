d1a69bbac361db4d77f1301f718a3f6012e84054
Matt Eva <matteweva@gmail.com>
2026-01-10 15:52:03 -0800

    saving progress on csrf loading

--- src/app/Test.jsx
+++ src/app/Test.jsx
@@ -1,23 +1,51 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Test({ hello }) {
+  const [csrfToken, setCsrfToken] = useState(null);
   //   throw new Error("problem!");
+
+  const apiClient = axios.create({
+    withCredentials: true,
+  });
 
   useEffect(() => {
     getHello();
+    getCsrfToken();
   }, []);
+
+  async function getCsrfToken() {
+    try {
+      const res = await apiClient.get(
+        process.env.NEXT_PUBLIC_API + "/get-csrf",
+        {
+          credentials: "include",
+        }
+      );
+      if (res.statusText === "OK") {
+        const token = res.headers["x-csrf-token"];
+        setCsrfToken(token);
+        apiClient.defaults.headers.common["X-CSRF-Token"] = token;
+      } else {
+        console.log(res);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   async function getHello() {
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_API + "/hello");
-      if (res.ok) {
-        const data = await res.json();
-        console.log(data);
+      const res = await apiClient(process.env.NEXT_PUBLIC_API + "/hello");
+      console.log(res);
+      if ((res.statusText = "OK")) {
+        // const data = await res.json();
+        console.log(res.data);
       } else {
-        const error = await res.text();
-        throw new Error(error);
+        // const error = await res.text();
+        console.log(res);
       }
     } catch (e) {
       console.error(e);
@@ -26,7 +54,7 @@ function Test({ hello }) {
 
   async function postHello() {
     try {
-      const res = await fetch(process.env.NEXT_PUBLIC_API + "/hello", {
+      const res = await apiClient.post(process.env.NEXT_PUBLIC_API + "/hello", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
