e7aa29f9ab95491a2a9c3c14f9df921bc710c8a3
Matt Eva <matteweva@gmail.com>
2026-02-11 15:53:31 -0800

    filling out library subpages

---
+++ src/app/library/my-writing/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyWriting() {
+  return <div>MyWriting</div>;
+}
+
+export default MyWriting;
---
+++ src/app/library/past-reads/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PastReads() {
+  return <div>PastReads</div>;
+}
+
+export default PastReads;
